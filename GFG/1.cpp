dc78168b21929487701b88b92a1d205cf06f14f1
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-07-24 16:32:06 +0530

    Update 1.cpp

--- GFG/1.cpp
+++ GFG/1.cpp
@@ -1,3 +1,76 @@
+//https://practice.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1?page=1&category[]=sliding-window&sortBy=submissions
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr , int N){
+        
+        long long sum=0,maxsum=0;
+        //first window
+        for(int i=0;i<K;i++)
+        {
+            sum+=Arr[i];
+        }
+        
+        maxsum=sum;
+        //subsequent windows
+        for(int i=0,j=K;j<N;j++,i++)
+        {
+            sum+=Arr[j]-Arr[i];
+            
+            maxsum=max(maxsum,sum);
+        }
+        return maxsum;
+        
+        // code here 
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Given an array of integers of size ‘n’, 
 //Our aim is to calculate the maximum sum of ‘k’ consecutive elements 
 //in the array.
